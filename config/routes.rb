4a95a82d890d2f7e4002a7285a285b82838da5ee
eyiwarren <eyiwarren@gmail.com>
2019-01-18 01:31:02 -0600

    Final save to checkpoint-rails-CRUD

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
-  get 'welcome/index'
-  get 'welcome/about'
-  get 'welcome/contact'
-  get 'welcome/faq'
+  # #1
+
+  resources :posts
+  get 'about' => 'welcome#about'
+
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 root 'welcome#index'
 end
