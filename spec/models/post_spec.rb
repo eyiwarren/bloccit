012389c8f67278890e77417b5298c233c7fe5dfb
eyiwarren <eyiwarren@gmail.com>
2019-02-17 09:40:38 -0600

    Final save to checkpoint-rails-validation

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -11,7 +11,19 @@ RSpec.describe Post, type: :model do
  # #4
    let(:post) { topic.posts.create!(title: title, body: body) }
 
-   it { is_expected.to belong_to(:topic) }
+   #it { is_expected.to belong_to(:topic) }
+
+
+   describe "associations" do
+     it { expect(post).to respond_to(:topic) }
+   end
+
+   it { is_expected.to validate_presence_of(:title) }
+   it { is_expected.to validate_presence_of(:body) }
+   it { is_expected.to validate_presence_of(:topic) }
+
+   it { is_expected.to validate_length_of(:title).is_at_least(5) }
+   it { is_expected.to validate_length_of(:body).is_at_least(20) }
 
 
   describe "attributes" do
