a880d24aab9ad0530314deefbaef3ff64d84037e
eyiwarren <eyiwarren@gmail.com>
2019-02-23 02:22:42 -0600

    Final commit to checkpoint-rails-Auth-signingup-assignment

--- config/routes.rb
+++ config/routes.rb
@@ -8,8 +8,10 @@ Rails.application.routes.draw do
   resources :posts, except: [:index]
   resources :sponsored_posts, except: [:index]
  end
- 
+
   resources :users, only: [:new, :create]
+
+  post 'users/confirm' => 'users#confirm'
   get 'about' => 'welcome#about'
 
 
